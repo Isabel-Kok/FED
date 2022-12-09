8c3d1a69fef6aeafd354c67d5f3665cdf7ad4f59
Isabel-Kok <isabelkok2004@gmail.com>
2022-12-09 15:12:30 +0100

    hihi

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
-// JavaScript Document
-console.log("hi");
+
+
+
+var hamburgeropen = document.querySelector('nav:first-of-type>img:first-of-type');
+var hamburgersluit = document.querySelector('.hamburger img:first-of-type');
+var menu = document.querySelector('.hamburger');
+
+function openmenu(){
+    menu.classList.add('open')
+}
+function sluitmenu(){
+    menu.classList.remove('open')
+}
+
+hamburgeropen.addEventListener('click', openmenu);
+hamburgersluit.addEventListener('click', sluitmenu);
+
+console.log(hamburgeropen)
