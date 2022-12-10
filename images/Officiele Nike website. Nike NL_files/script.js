8c6c2006eb3fcb5fe87eba556031dd5c3dee2e8d
Isabel-Kok <isabelkok2004@gmail.com>
2022-12-10 20:55:57 +0100

    :)

---
+++ images/Officiele Nike website. Nike NL_files/script.js
@@ -0,0 +1,18 @@
+
+
+
+var hamburgeropen = document.querySelector('nav:first-of-type>img:first-of-type');
+var hamburgersluit = document.querySelector('.hamburger img:first-of-type');
+var menu = document.querySelector('.hamburger');
+
+function openmenu(){
+    menu.classList.add('open')
+}
+function sluitmenu(){
+    menu.classList.remove('open')
+}
+
+hamburgeropen.addEventListener('click', openmenu);
+hamburgersluit.addEventListener('click', sluitmenu);
+
+console.log(hamburgeropen)
